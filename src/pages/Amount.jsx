c693d5389eca77d09816d78a84239a3d3959cbb0
amrutharaj20393 <amrutharaj20393@gmail.com>
2025-10-25 22:04:04 +0530

    design

--- src/pages/Amount.jsx
+++ src/pages/Amount.jsx
@@ -6,26 +6,31 @@ function Amount() {
 
     const makePayment = async (data) => {
         console.log(data)
-            const result = await amountApi({data})
-            console.log(result)
-            window.location.href = result.data.url; 
+        const result = await amountApi({ data })
+        console.log(result)
+        window.location.href = result.data.url;
 
-        
+
 
         ///console.log(data)
 
     }
 
-   
+
     return (
         <>
             <div>
-                <h1>Amrutha</h1>
-                <div className='md:flex justify-center items-center  px-5 py-10 w-100'>
+                <h1 className='text-red-500 text-3xl'>Task By:Amrutha Raj</h1>
+                <div >
+                    <div>
+                        <h2 className='text-emerald-600 text-bold mx-5 my-2'> Amount</h2>
+                    </div>
+                    <div className='md:flex justify-center items-center  px-5  w-100'>
+                        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount" className='me-2 p-1' />
+                        <button type="button" className='bg-emerald-800 text-white px-5 p-1 rounded  hover:bg-white hover:border border-emerald-500 hover:text-emerald-600 w-full' onClick={() => { makePayment(amount) }}>Pay Now</button>
+                    </div>
 
-                    <h2>Enter Amount:</h2>
-                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount" />
-                    <button onClick={()=>{makePayment(amount)}}>Pay Now</button>
+
                 </div>
             </div>
 
